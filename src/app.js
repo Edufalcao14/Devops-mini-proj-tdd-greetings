344178a1835cbe1d3d05f3d119bd26ad395a79c7
edufalcao14 <eduardo.sampaio@student.vinci.be>
2023-11-10 14:00:05 +0100

    Feature : greeting function with 2 names

--- src/app.js
+++ src/app.js
@@ -1,15 +1,32 @@
 /* eslint-disable linebreak-style */
 
-function greet(name) {
-    if (name === undefined || name === null || name.length === 0) {
-        return 'Hello, my friend.';
+function greet(names) {
+    if (names.length === 1 || names.length ===0) {
+        if (
+            names[0] === undefined ||
+            names[0] === '' ||
+            names[0] === null ||
+            names.length === 0
+        ) {
+            return 'Hello, my friend.';
+        }
+
+        let upperName = names[0].toUpperCase();
+
+        if (upperName === names[0]) {
+            return 'HELLO, ' + upperName + '!';
+        } else {
+            return 'Hello ' + names[0] + ' !';
+        }
+    }else if (names.length === 2) {
+        return greet2Names(names);
     }
-    let upperName = name.toUpperCase();
-    if (upperName === name) {
-        return 'HELLO, ' + upperName + '!';
-    } else {
-        return 'Hello ' + name + ' !';
-    }
+}
+
+
+
+function greet2Names(names) {
+    return "Hello, " +  names[0] + " and " + names[1] + ".";
 }
 
 module.exports = greet;
--- src/app.test.js
+++ src/app.test.js
@@ -2,16 +2,21 @@
 const greet = require('./app');
 
 test('should return an greet with the correct name ', () => {
-    const result = greet("Bob");
+    const result = greet(["Bob"]);
     expect(result).toEqual("Hello Bob !");
 });
 
 test('should return : Hello, my friend.', () => {
-    const result = greet();
+    const result = greet([""]);
     expect(result).toEqual("Hello, my friend.");
 });
 
 test('should return : HELLO, JERRY!', () => {
-    const result = greet("JERRY");
+    const result = greet(["JERRY"]);
     expect(result).toEqual("HELLO, JERRY!");
+});
+
+test('should return : Hello, Jill and Jane.', () => {
+    const result = greet( ["Jill", "Jane"],);
+    expect(result).toEqual("Hello, Jill and Jane.");
 });
