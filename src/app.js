fcf2701cbfb35a5a375946c69429f508350dee53
Tom Simonis <tom.simonis@student.vinci.be>
2023-11-10 13:39:42 +0100

    Feature : Greeting function without name

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 /* eslint-disable linebreak-style */
 
-function greet(name) { 
-    return "Hello " + name + " !";
+function greet(name) {
+    if (name === undefined || name === null || name.length === 0) {
+        return "Hello, my friend.";
+    } else {
+        return "Hello " + name + " !";
+    }
 }
 
 module.exports = greet;
--- src/app.test.js
+++ src/app.test.js
@@ -5,3 +5,8 @@ test('should return an greet with the correct name ', () => {
     const result = greet("Bob");
     expect(result).toEqual("Hello Bob !");
 });
+
+test('should return : Hello, my friend.', () => {
+    const result = greet();
+    expect(result).toEqual("Hello, my friend.");
+});
